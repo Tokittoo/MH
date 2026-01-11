314d56cfd3bc5bfee48581aab08ee3926216ada5
tokittoo <krishnaprasath.7x@gmail.com>
2026-01-11 22:03:02 +0530

    Theme Change according to the time

--- components/theme-provider.tsx
+++ components/theme-provider.tsx
@@ -4,8 +4,36 @@ import * as React from 'react'
 import {
   ThemeProvider as NextThemesProvider,
   type ThemeProviderProps,
+  useTheme,
 } from 'next-themes'
 
+function ThemeWatcher() {
+  const { setTheme } = useTheme()
+
+  React.useEffect(() => {
+    const updateThemeBasedOnTime = () => {
+      const hour = new Date().getHours()
+      // Day time: 6 AM (inclusive) to 6 PM (exclusive) -> 06:00 to 17:59
+      const isDay = hour >= 6 && hour < 18
+      setTheme(isDay ? 'light' : 'dark')
+    }
+
+    updateThemeBasedOnTime()
+
+    // Check periodically (every minute)
+    const interval = setInterval(updateThemeBasedOnTime, 60000)
+
+    return () => clearInterval(interval)
+  }, [setTheme])
+
+  return null
+}
+
 export function ThemeProvider({ children, ...props }: ThemeProviderProps) {
-  return <NextThemesProvider {...props}>{children}</NextThemesProvider>
+  return (
+    <NextThemesProvider {...props}>
+      <ThemeWatcher />
+      {children}
+    </NextThemesProvider>
+  )
 }
